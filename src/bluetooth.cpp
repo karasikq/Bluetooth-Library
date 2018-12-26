ffda6f597fcf2fcd0e1ecf4ad62adb0293600864
karasikq <uzzet@users.noreply.github.com>
2018-12-26 12:47:45 +0300

    Fix baudrate problem

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1,181 +1,175 @@
 #include "bluetooth.h"
 
-Bluetooth::Bluetooth(unsigned short rx, unsigned short tx, unsigned int br)
+Bluetooth::Bluetooth(unsigned short rx, unsigned short tx, long br)
 {
-    RX = rx;
-    TX = tx;
-    baudrate = br;
-    device = new SoftwareSerial(RX, TX);
+  RX = rx;
+  TX = tx;
+  baudrate = br;
+  device = new SoftwareSerial(RX, TX);
+
+  memset(message, '1', 51);
 }
 
 Bluetooth::Bluetooth(unsigned short rx, unsigned short tx)
 {
-    RX = rx;
-    TX = tx;
-    baudrate = deffaultBaudrate;
-    device = new SoftwareSerial(RX, TX);
+  RX = rx;
+  TX = tx;
+  baudrate = deffaultBaudrate;
+  device = new SoftwareSerial(RX, TX);
+
+  memset(message, '1', 51);
 }
 
 Bluetooth::~Bluetooth()
 {
-    delete device;
+  delete device;
 }
 
 void Bluetooth::begin()
 {
-    device->begin(baudrate);
-    delay(waitTime);
-    init = true;
-    setBaudrate(baudrate);
-    clearSerial();
-    Serial.write("Ready.");
+  device->begin(baudrate);
+  delay(waitTime);
+  init = true;
+  setBaudrate(baudrate);
+  clearSerial();
+  Serial.write("Ready.");
 }
 
 void Bluetooth::reset()
 {
-    if (init) 
-    {
-        strcpy(message, "AT+RESET");
-        write(message);
-    }
+  if (init)
+  {
+    strcpy(message, "AT+RESET");
+    write(message);
+  }
 }
 
 void Bluetooth::write(const char buffer[])
 {
-    device->write(buffer);
-    delay(waitTime);
+  device->write(buffer);
+  delay(waitTime);
 }
 
 void Bluetooth::clearSerial()
 {
-    char garbage; 
-    while (device->available())
-        garbage = device->read();
+  char garbage;
+  while (device->available())
+    garbage = device->read();
 }
 
-void Bluetooth::receive() 
+void Bluetooth::receive()
 {
-    int index = 0;
-    if(device->available())
-        strcpy(message, "");
-    while(device->available()) 
-    {
-        message[index] = device->read();
-        index++;
-    }
-    message[index] = '\0';
+  int index = 0;
+  if (device->available())
+    memset(message, '1', 51);
+  while (device->available())
+  {
+    message[index] = device->read();
+    index++;
+  }
+  message[index] = '\0';
 }
 
 void Bluetooth::setDeviceName(const char name[])
 {
-    if (strlen(name) <= nameSize) 
-    {
-        strcpy(message, "AT+NAME");
-        strcat(message, name);
-        strcpy(deviceName, name);
-        write(message);
-    }
+  if (strlen(name) <= nameSize)
+  {
+    strcpy(message, "AT+NAME");
+    strcat(message, name);
+    strcpy(deviceName, name);
+    write(message);
+  }
 }
 
 void Bluetooth::setDevicePin (const char pin[]) {
-    if (strlen(pin) == 4) {
-        for (int c = 0; c < 4; c++) {
-            if (pin[c] < 0x30 || pin[c] > 0x90) {
-                return;
-            }
-        }
-        strcpy(message, "AT+PIN");
-        strcat(message, pin);
-        strcpy(devicePin, pin);
-        write(message);
+  if (strlen(pin) == 4) {
+    for (int c = 0; c < 4; c++) {
+      if (pin[c] < 0x30 || pin[c] > 0x90) {
+        return;
+      }
     }
+    strcpy(message, "AT+PIN");
+    strcat(message, pin);
+    strcpy(devicePin, pin);
+    write(message);
+  }
 }
 
 char* Bluetooth::getMessage()
 {
-    receive();
-    return message;
+  receive();
+  return message;
 }
 
 char* Bluetooth::getName()
 {
-    return deviceName;
+  return deviceName;
 }
 
 char* Bluetooth::getPin()
 {
-    return devicePin;
+  return devicePin;
 }
 
-unsigned short Bluetooth::getRXPin() 
+unsigned short Bluetooth::getRXPin()
 {
-    return RX;
+  return RX;
 }
 
-unsigned short Bluetooth::getTXPin () 
+unsigned short Bluetooth::getTXPin()
 {
-    return TX;
+  return TX;
 }
 
-void Bluetooth::setBaudrate (unsigned short tier)
+void Bluetooth::setBaudrate(const long br)
 {
-    if (init) 
+  if (init)
+  {
+    strcpy(message, "AT+BAUD");
+    baudrate = br;
+    switch (br)
     {
-        strcpy(message, "AT+BAUD");
-        switch (tier) {
-            case 1:
-                strcat(message, "1");
-                baudrate = 1200;
-                break;
-            case 2:
-                strcat(message, "2");
-                baudrate = 2400;
-                break;
-            case 3:
-                strcat(message, "3");
-                baudrate = 4800;
-                break;
-            case 4:
-                strcat(message, "4");
-                baudrate = 9600;
-                break;
-            case 5:
-                strcat(message, "5");
-                baudrate = 19200;
-                break;
-            case 6:
-                strcat(message, "6");
-                baudrate = 38400;
-                break;
-            case 7:
-                strcat(message, "7");
-                baudrate = 57600;
-                break;
-            case 8:
-                strcat(message, "8");
-                baudrate = 115200;
-                break;
-            case 9:
-                strcat(message, "9");
-                baudrate = 230400;
-                break;
-            case 10:
-                strcat(message, "10");
-                baudrate = 460800;
-                break;
-            case 11:
-                strcat(message, "11");
-                baudrate = 921600;
-                break;
-            case 12:
-                strcat(message, "12");
-                baudrate = 1382400;
-                break;
-            default:
-                strcat(message, "4");
-                baudrate = deffaultBaudrate;
-                break;
-        }
-        write(message);
+      case 1200:
+        strcat(message, "1");
+        break;
+      case 2400:
+        strcat(message, "2");
+        break;
+      case 4800:
+        strcat(message, "3");
+        break;
+      case 9600:
+        strcat(message, "4");
+        break;
+      case 19200:
+        strcat(message, "5");
+        break;
+      case 38400:
+        strcat(message, "6");
+        break;
+      case 57600:
+        strcat(message, "7");
+        break;
+      case 115200:
+        strcat(message, "8");
+        break;
+      case 230400:
+        strcat(message, "9");
+        break;
+      case 460800:
+        strcat(message, "10");
+        break;
+      case 921600:
+        strcat(message, "11");
+        break;
+      case 1382400:
+        strcat(message, "12");
+        break;
+      default:
+        strcat(message, "4");
+        baudrate = deffaultBaudrate;
+        break;
     }
+    write(message);
+  }
 }
--- src/bluetooth.h
+++ src/bluetooth.h
@@ -1,9 +1,9 @@
 /*
- * Bluetooth library
- * Author: https://github.com/karasikq
- * Last update: 25.12.2018
- * License: GNU GPLv3
- */
+   Bluetooth library
+   Author: https://github.com/karasikq
+   Last update: 25.12.2018
+   License: GNU GPLv3
+*/
 
 
 #ifndef _BLUETOOTH_H_
@@ -13,39 +13,39 @@
 #include <SoftwareSerial.h>
 
 const unsigned short nameSize = 40;
-const unsigned int deffaultBaudrate = 9600;
-const unsigned int waitTime =	1200;
+const long deffaultBaudrate = 9600;
+const unsigned int waitTime =  2000;
 const unsigned short messageSize = 10;
 
-class Bluetooth 
+class Bluetooth
 {
-private:
-		SoftwareSerial	*device;
-		bool init = false;
-		unsigned short RX;
-		unsigned short TX;
-		unsigned int baudrate;
+  private:
+    SoftwareSerial  *device;
+    bool init = false;
+    unsigned short RX;
+    unsigned short TX;
+    long baudrate;
     char message[messageSize + nameSize + 1];
-		char deviceName[nameSize + 1];
-		char devicePin[4 + 1];
-		void clearSerial();
+    char deviceName[nameSize + 1];
+    char devicePin[4 + 1];
+    void clearSerial();
     void receive();
     void write(const char buffer[]);
-public:
-    Bluetooth(unsigned short rx, unsigned short tx, unsigned int br);
+  public:
+    Bluetooth(unsigned short rx, unsigned short tx, long br);
     Bluetooth(unsigned short rx, unsigned short tx);
-		~Bluetooth();
-   
-		void begin();
-		void reset();
-		void setBaudrate(unsigned short tier);
-		void setDeviceName(const char name[]);
-		void setDevicePin(const char pin[]);
+    ~Bluetooth();
+
+    void begin();
+    void reset();
+    void setBaudrate(const long tier);
+    void setDeviceName(const char name[]);
+    void setDevicePin(const char pin[]);
     char* getMessage();
-    char* getName();
+    char* getName();;
     char* getPin();
-		unsigned short getRXPin();
-		unsigned short getTXPin();
+    unsigned short getRXPin();
+    unsigned short getTXPin();
 };
 
 #endif
